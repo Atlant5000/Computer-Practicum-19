e70b0855876dc0e1afeb04fe23f35569eb7fd568
Максим <maximmmd13@gmail.com>
2025-02-02 20:35:59 +0200

    Task 4

---
+++ ConsoleApp33/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp33
+{
+    /// <summary>
+    ///  Составьте программу вычисления суммы 150 первых членов арифметической прогрессии, если a1= –200; d = 0,2.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = -200;
+            double d = 0.2;
+            int count = 1;
+            double sum = 0;
+            while (count <= 150)
+            {
+                a += d;
+                count++;
+                sum += a;
+            }
+            Console.WriteLine($"Сумма={sum}");
+            Console.ReadKey();
+        }
+    }
+}
